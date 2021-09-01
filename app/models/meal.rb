7209d6ee4a8c97c8bd9af0c10351a2ed124b5d7a
Indira Vanhulle <indira_vanhulle@hotmail.com>
2021-09-01 17:55:58 +0200

    seed-faker4

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,8 +1,9 @@
 class Meal < ApplicationRecord
   has_many :food_category_meals
-  has_many :meals_ingredients
+  has_many :meals_ingredients, dependent: :destroy
   has_many :ingredients, through: :meals_ingredients
+  has_many :food_categories, through: :food_category_meals
   has_many :order_meals
   belongs_to :restaurant
-  monetize :price_cents
+  # monetize :price_cents
 end
