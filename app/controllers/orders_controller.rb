cd5f69ea68edc31127fcf3ec6aebfd5f718455b7
Franck Dirat <franck.dirat@gmail.com>
2021-09-01 14:23:53 +0200

    stripe ongoing

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -16,22 +16,22 @@ class OrdersController < ApplicationController
     @order.status = 'pending'
     # @order.amount = @meal.price
 
-    session = Stripe::Checkout::Session.create(
-      payment_method_types: ['card'],
-      line_items: [{
-        # name: @meal.name,
-        # images: [@meal.photo_url],
-        # amount: @meal.price_cents,
-        currency: 'eur',
-        quantity: 1
-      }],
-      # success_url: order_url(order),
-      # cancel_url: order_url(order)
-    )
+    # session = Stripe::Checkout::Session.create(
+    #   payment_method_types: ['card'],
+    #   line_items: [{
+    #     name: @meal.name,
+    #     images: [@meal.photo_url],
+    #     amount: @meal.price_cents,
+    #     currency: 'eur',
+    #     quantity: 1
+    #   }],
+    #   success_url: order_url(order),
+    #   cancel_url: order_url(order)
+    # )
 
     if @order.save
-      order.update(checkout_session_id: session.id)
-      redirect_to new_order_payment_path(order), notice: 'Order was successfully created.'
+      # @order.update(checkout_session_id: session.id)
+      redirect_to new_order_payment_path(@order), notice: 'Order was successfully created.'
     else
       render :new
     end
