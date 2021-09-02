300d8cc357bb43073be14e15e80773966ea8b580
Franck Dirat <franck.dirat@gmail.com>
2021-09-02 18:03:57 +0200

    overpaste

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -14,25 +14,26 @@ class OrdersController < ApplicationController
     @order.user = current_user
     @order.status = 'pending'
     # @order.amount = @meal.price
-    @order.save
-    @order.meals_proposition
-    meals_count = Hash.new(0)
-    meals = []
 
+    @meal = Meal.create!(name: "Pizza", description: "Pizza tomato and cheese", price: 15, restaurant_id: 1)
+    if @order.save
+      @order.meals << @meal
+      # @order.meals_proposition
+      # meals_count = Hash.new(0)
+      # meals = []
 
-
-    session = Stripe::Checkout::Session.create(
-      payment_method_types: ['card'],
-      line_items: [{
-        name: @meal.name,
-        description: @meal.description,
-        amount: @meal.price_cents,
-        currency: 'eur',
-        quantity: 1
-      }],
-      success_url: order_url(@order),
-      cancel_url: order_url(@order)
-    )
+      session = Stripe::Checkout::Session.create(
+        payment_method_types: ['card'],
+        line_items: [{
+          name: @meal.name,
+          description: @meal.description,
+          amount: @meal.price_cents,
+          currency: 'eur',
+          quantity: 1
+        }],
+        success_url: order_url(@order),
+        cancel_url: order_url(@order)
+      )
 
       @order.update(checkout_session_id: session.id, amount: @meal.price_cents*0.01)
       redirect_to new_order_payment_path(@order), notice: 'Order was successfully created.'
