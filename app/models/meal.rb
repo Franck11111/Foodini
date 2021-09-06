bcd5bdb159f0ceefc89036acb71dae15a99390d6
Marco Kjeldsen <marcojmk@tuta.io>
2021-09-06 11:30:52 +0200

    added dependent, destroy to run db:seed

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -3,8 +3,9 @@ class Meal < ApplicationRecord
   has_many :meals_ingredients, dependent: :destroy
   has_many :ingredients, through: :meals_ingredients
   has_many :food_categories, through: :food_category_meals
-  has_many :order_meals
+  has_many :order_meals, dependent: :destroy
   belongs_to :restaurant
   monetize :price_cents
   has_many :food_categories, through: :food_category_meals
+
 end
