22f7435093aa937025cfc3411c079f24a76f250d
Marco Kjeldsen <marcojmk@tuta.io>
2021-09-06 14:47:16 +0200

    searchbar stuff

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -34,15 +34,18 @@ import { selectFoodTypes } from '../components/select_food_types';
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
-  selectFoodTypes();
+
 
 
 });
 
 //import navbar
 import { initUpdateNavbarOnScroll } from "../components/navbar";
+import { initAutocomplete } from "../plugins/init_autocomplete";
 
 document.addEventListener("turbolinks:load", () => {
   // Call your JS functions here
   initUpdateNavbarOnScroll();
+  selectFoodTypes();
+  initAutocomplete();
 });
